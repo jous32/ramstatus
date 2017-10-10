9d59e8382094d42159038694d3e7bbc079fd1fb3
Jose Vasquez <jous32@gmail.com>
2017-10-10 14:25:35 -0500

    make a post

--- get_ram.rb
+++ get_ram.rb
@@ -1,3 +1,31 @@
 require 'systeminformation'
+require 'net/http'
+require 'uri'
+require 'json'
 
-puts SystemInformation.memory[:free]
+uri = URI.parse("http://ramstatus.herokuapp.com/memories")
+
+header = {'Content-Type': 'text/json'}
+#puts SystemInformation.memory
+memory_free = SystemInformation.memory[:free]
+memory_used = SystemInformation.memory[:used]
+
+memories_obj = {memories: {
+                   free: memory_free,
+                   used: memory_used
+                      }
+            }
+
+# Create the HTTP objects
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri, header)
+request.body = memories_obj.to_json
+
+# Send the request
+response = http.request(request)
+
+puts "response #{response}"
+
+response.each do |key, value|
+  puts "#{key}:#{value}"
+end
